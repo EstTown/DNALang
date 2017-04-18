c139c564f0776f64e384ecb2c85bf6c610442272
Dascham <sydowtwenty@gmail.com>
2017-04-18 14:27:24 +0200

    Made Main great again (also made a main class)

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+import java.io.FileInputStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+/**
+ * Created by Ruben on 18-04-2017.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //stuff
+        //String a = "sadkjasdkj";
+        //char[] b = new char[]{'g', 'd', 'h', 'h'};
+
+        try{
+            InputStream stream = new FileInputStream("Input");
+
+            ANTLRInputStream input = new ANTLRInputStream(stream);
+
+            languageLexer lexer = new languageLexer(input);
+
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+            languageParser parser = new languageParser(tokens);
+
+            ParseTree tree = parser.prog();
+
+            System.out.print(tree.toStringTree(parser)); //print tree as text
+        }
+        catch(Exception parser){
+            System.out.print("Didn't work!");
+        }
+
+    }
+}
