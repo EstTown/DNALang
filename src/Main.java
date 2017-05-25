a82a1bb717383d7c34a780b2f9a28e678a54dbab
Frederik <f_hansen@live.dk>
2017-05-25 15:08:39 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-/*
+
     	int number = 3;
         ParseTester parseTester = new ParseTester();
         //parseTester.ParseAllInputs(number);
         parseTester.ParseSpecificInput(number);
-*/
+
         //ParseTreeGUI ptg = new ParseTreeGUI();
         //ptg.Show(number); //specify input file by giving an integer
 
 		//NO REMOVE THIS
 		//This is what we use in the end, to kick the whole thing off!
-		Parser compiler = new Parser(args);
-		compiler.Start();
+		//Parser compiler = new Parser(args);
+		//compiler.Start();
 	}
 }
